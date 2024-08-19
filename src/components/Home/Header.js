40c6b524dda88516130604030fa687d5a8365e09
Lucas Ribeiro <lucasr.camargo@live.com>
2024-08-19 18:48:39 -0300

    chore: Add configuration files and global styles

---
+++ src/components/Home/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-300 p-4">
+      <h2 className="text-center text-black font-verdana text-xl">
+        Calculadora de Horas
+      </h2>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Header from "../components/Home/Header";
+
+const Index = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Index;
